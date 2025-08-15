1cf02696eab3576c4a231c0bd2e4fdfa7e6e2afa
Malek Brachemi <brachemi.malek92@gmail.com>
2025-08-15 05:42:45 -0400

    Fetch Object class and wired method

--- force-app/main/default/lwc/bulkRecordUpdater/bulkRecordUpdater.js
+++ force-app/main/default/lwc/bulkRecordUpdater/bulkRecordUpdater.js
@@ -1,7 +1,31 @@
 import { LightningElement,wire,api } from 'lwc';
+import getAllSalesforceObjects from '@salesforce/apex/BulkRecordUpdaterController.getAllSalesforceObjects';
 
 export default class BulkRecordUpdater extends LightningElement {
 
+    salesforceObjectOptions = []
 
+    @wire(getAllSalesforceObjects)
+    wiredObject({data,error}){
+        if(data){
+            this.salesforceObjectOptions = data.map(obj => ({
+                label: obj.Label,
+                value: obj.QualifiedApiName
+            }))
+        }
+        if(error){
+            console.error(error)
+        }
+    }
 
+    handleChangeObject(event){
+
+    }
+
+    handleClear(){
+
+    }
+    handleUpdate(){
+
+    }
 }
