660a26faf1e1a1f6593da857517f0d21089e8726
Malek Brachemi <brachemi.malek92@gmail.com>
2025-08-18 13:41:34 -0400

    Get Updatable fields , restructured the js file

--- force-app/main/default/lwc/bulkRecordUpdater/bulkRecordUpdater.js
+++ force-app/main/default/lwc/bulkRecordUpdater/bulkRecordUpdater.js
@@ -1,19 +1,47 @@
 import { LightningElement,wire,api } from 'lwc';
 import getAllSalesforceObjects from '@salesforce/apex/BulkRecordUpdaterController.getAllSalesforceObjects';
-import getAllFields from '@salesforce/apex/BulkRecordUpdaterController.getAllFields';
+import getAllRecords from '@salesforce/apex/BulkRecordUpdaterController.getAllRecords';
+import getFieldsEditable from '@salesforce/apex/BulkRecordUpdaterController.getFieldsEditable';
 
 export default class BulkRecordUpdater extends LightningElement {
 
+    // ----------------------
+    // Properties
+    // ----------------------
+
     salesforceObjectOptions = []
     objectFieldsOptions = []
-    selectedObject
-    selectedField
-    error 
+    records = []
     isLoading = true
- 
+    min = 1
+    max = 10
+    selectedObject
+    selectedFields
+    error 
+
+
+    // ----------------------
+    // Getters
+    // ----------------------
+
     get isUpdateDisabled(){
-        return !this.selectedObject || !this.selectedField
+        return !this.selectedObject || !this.selectedFields
     }
+
+    get columns(){
+        if(!this.selectedFields) return []
+        return this.selectedFields.map(field =>({
+            label : field,
+            fieldName : field,
+            editable : true
+
+        }))
+    }
+
+    // ----------------------
+    // Wires
+    // ----------------------
+
     @wire(getAllSalesforceObjects)
     wiredObject({data,error}){
         if(data){
@@ -26,44 +54,67 @@ export default class BulkRecordUpdater extends LightningElement {
             console.error(error)
         }
         this.isLoading = false
+     }
 
+
+     @wire(getFieldsEditable, { objName: '$selectedObject' })
+     wiredFields({ data, error }) {
+        this.isLoading = true
+
+        // console.log(JSON.stringify(data));
+        console.log('DATE : ',data);
+         if (data) {
+            console.log('Here')
+             this.objectFieldsOptions = data; // Already in correct format
+             console.log(this.objectFieldsOptions);
+         }
+         if (error) {
+            console.log('There')
+
+             console.error(error);
+         }
+         this.isLoading = false
+
+     }
+
+    // ----------------------
+    // Apex Calls
+    // ----------------------
+
+    async fetchRecords(objName,fields){
+        console.log('objName ',objName)
+        console.log('fields ',fields)
+        if(!fields || !objName) return;
+        try{
+            this.records = await getAllRecords({objName :objName, fields: JSON.stringify(fields)});  
+        }
+        catch{
+
+        }
     }
+
+
+    // ----------------------
+    // Event Handlers
+    // ----------------------
+     
 
     handleChangeObject(event){
         this.isLoading = true
         this.selectedObject = event.detail.value
         console.log(this.selectedObject)
+        //this.fetchObjectFields()
+    }    
 
-        this.fetchObjectFields()
-    }
-    handleSelectFields(event){
-        this.selectedField = event.detail.value
-        console.log(this.selectedField)
-    }
-
-    async fetchObjectFields(){
-        if(!this.selectedObject) return;
-        try{
-            const fields = await getAllFields({objName : this.selectedObject})
-            this.objectFieldsOptions = fields.map(field =>({
-                label: field.Label,
-                value: field.QualifiedApiName
-            }))
-            console.log(JSON.stringify(this.objectFieldsOptions))
-            this.error = undefined;
-        }
-        catch (error){
-            this.objectFieldsOptions = [];
-            this.error = error;
-        }
-        finally{
-            this.isLoading = false
-        }
-
-    }
-
+    handleChangeFields(event){
+        this.selectedFields = event.detail.value
+        console.log(this.selectedFields)
+        this.fetchRecords(this.selectedObject,this.selectedFields)
+    }    
+ 
+ 
     handleClear(){
         this.selectedObject = null;
-        this.selectedField = null
+        this.selectedFields = null
     }
 }
